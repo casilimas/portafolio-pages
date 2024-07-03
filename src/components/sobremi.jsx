6ffd3da6aff0f2a3343dcd808aca0852f6a4b27c
freddy andres casilimas sanchez <tu_correo@example.com>
2024-07-03 18:56:51 -0400

    Actualizar ruta del archivo curriculun.pdf en SobreMi.js

--- src/components/sobremi.jsx
+++ src/components/sobremi.jsx
@@ -18,7 +18,7 @@ const SobreMi = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-           <img
+          <img
             src={linkedinIcon}
             alt="LinkedIn"
             className="linkedin-icon"
@@ -40,53 +40,50 @@ const SobreMi = () => {
         </a>
       </div>
       <div className="caja-blanca">
-      <img src={fotoPerfilDos} alt="Foto de Freddy Casilimas" className="foto-perfil-dos" />
-      <div className="texto-sobremi">
-        <h1>Sobre mí</h1>
-       
-        <h2>Freddy Casilimas</h2>
-        <p>
-          Soy Desarrollador Web Full Stack con una fuerte pasión por la tecnología y la programación. He adquirido experiencia a través de proyectos personales, utilizando tecnologías como React, Node.js.
-        </p>
-        <h3>Experiencia Profesional</h3>
-        <p>
-          Aunque no poseo un título formal en desarrollo web, he trabajado en diversos proyectos que han enriquecido mis habilidades y conocimientos en este campo.
-        </p>
-        <ul>
-          
-          <li>
-            <strong>Proyecto Personal</strong>: He creado una aplicación de gestión de tareas utilizando React y Node.js, la cual ha sido efectiva en sus pruebas.
-          </li>
-        </ul>
-        <h3>Habilidades</h3>
-        <ul>
-          <li>HTML, CSS, JavaScript</li>
-          <li>React</li>
-          <li>Node.js, Express</li>
-          <li>Python</li>
-          
-        </ul>
-        <h3>Proyectos Destacados</h3>
-        <ul>
-          <li>
-            <strong>Gestor de Tareas</strong>: Aplicación web para gestionar tareas, desarrollada con React y Node.js. [<a href="https://github.com/casilimas" target="_blank" rel="noopener noreferrer">Ver en GitHub</a>]
-          </li>
-          <li>
-            <strong>Portfolio Personal</strong>: Mi sitio web personal, donde muestro mis proyectos y habilidades. [<a href="https://github.com/casilimas" target="_blank" rel="noopener noreferrer">Ver en GitHub</a>]
-          </li>
-        </ul>
-        <h3>Objetivos</h3>
-        <p>
-          Mi objetivo es seguir desarrollándome como profesional en el campo del desarrollo web, aprendiendo nuevas tecnologías y enfrentando desafíos que me permitan crecer y contribuir al éxito de los proyectos en los que participe.
-        </p>
-        <h3>Contacto</h3>
-        <p>Email: contacto</p>
-        <p>
-          LinkedIn: <a href="https://www.linkedin.com/in/freddy-casilimas-38116314b/" target="_blank" rel="noopener noreferrer">Freddy Casilimas</a>
-        </p>
-        <p>
-          GitHub: <a href="https://github.com/casilimas" target="_blank" rel="noopener noreferrer">freddycasilimas</a>
-        </p>
+        <img src={fotoPerfilDos} alt="Foto de Freddy Casilimas" className="foto-perfil-dos" />
+        <div className="texto-sobremi">
+          <h1>Sobre mí</h1>
+          <h2>Freddy Casilimas</h2>
+          <p>
+            Soy Desarrollador Web Full Stack con una fuerte pasión por la tecnología y la programación. He adquirido experiencia a través de proyectos personales, utilizando tecnologías como React, Node.js.
+          </p>
+          <h3>Experiencia Profesional</h3>
+          <p>
+            Aunque no poseo un título formal en desarrollo web, he trabajado en diversos proyectos que han enriquecido mis habilidades y conocimientos en este campo.
+          </p>
+          <ul>
+            <li>
+              <strong>Proyecto Personal</strong>: He creado una aplicación de gestión de tareas utilizando React y Node.js, la cual ha sido efectiva en sus pruebas.
+            </li>
+          </ul>
+          <h3>Habilidades</h3>
+          <ul>
+            <li>HTML, CSS, JavaScript</li>
+            <li>React</li>
+            <li>Node.js, Express</li>
+            <li>Python</li>
+          </ul>
+          <h3>Proyectos Destacados</h3>
+          <ul>
+            <li>
+              <strong>Gestor de Tareas</strong>: Aplicación web para gestionar tareas, desarrollada con React y Node.js. [<a href="https://github.com/casilimas" target="_blank" rel="noopener noreferrer">Ver en GitHub</a>]
+            </li>
+            <li>
+              <strong>Portfolio Personal</strong>: Mi sitio web personal, donde muestro mis proyectos y habilidades. [<a href="https://github.com/casilimas" target="_blank" rel="noopener noreferrer">Ver en GitHub</a>]
+            </li>
+          </ul>
+          <h3>Objetivos</h3>
+          <p>
+            Mi objetivo es seguir desarrollándome como profesional en el campo del desarrollo web, aprendiendo nuevas tecnologías y enfrentando desafíos que me permitan crecer y contribuir al éxito de los proyectos en los que participe.
+          </p>
+          <h3>Contacto</h3>
+          <p>Email: contacto</p>
+          <p>
+            LinkedIn: <a href="https://www.linkedin.com/in/freddy-casilimas-38116314b/" target="_blank" rel="noopener noreferrer">Freddy Casilimas</a>
+          </p>
+          <p>
+            GitHub: <a href="https://github.com/casilimas" target="_blank" rel="noopener noreferrer">freddycasilimas</a>
+          </p>
         </div>
       </div>
     </div>
